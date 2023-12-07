6443c0667584d277ae4415fcc1c8e5273482bf3d
yurytrifonov <jutrifonov@uchicago.edu>
2023-12-07 14:21:06 -0600

    @yurytrifonov added the results for the last version analysis

--- experimental/mc.aggregate.R
+++ experimental/mc.aggregate.R
@@ -230,3 +230,103 @@ ggplot(graph.data, aes(x = s.size, y = ci.hit)) +
   geom_line(color = 'red') + geom_point(color = 'red') +
   labs(x = "Sample Size", y = "Coverage %") #+ scale_y_continuous(limits = c(0.6, 1))
 
+
+
+
+
+### v.4.7 version
+library(ggplot2)
+
+load('/Users/trifonovjuri/Desktop/pkg.sreg/sreg.git/data/unified/100.RData')
+tau <- na.omit(as.numeric(sapply(simres, function(simres) simres$tau)))
+se <- na.omit(sapply(simres, function(simres) simres$se))
+ci.hit <- na.omit(sapply(simres, function(simres) simres$ci.hit))
+tau.100 <- mean(tau)
+se.num.100 <- sd(tau)
+se.analyt.100 <- mean(se)
+ci.hit.100 <- mean(ci.hit)
+
+load('/Users/trifonovjuri/Desktop/pkg.sreg/sreg.git/data/unified/300.RData')
+tau <- na.omit(as.numeric(sapply(simres, function(simres) simres$tau)))
+se <- na.omit(sapply(simres, function(simres) simres$se))
+ci.hit <- na.omit(sapply(simres, function(simres) simres$ci.hit))
+tau.300 <- mean(tau)
+se.num.300 <- sd(tau)
+se.analyt.300 <- mean(se)
+ci.hit.300 <- mean(ci.hit)
+
+load('/Users/trifonovjuri/Desktop/pkg.sreg/sreg.git/data/unified/500.RData')
+tau <- na.omit(as.numeric(sapply(simres, function(simres) simres$tau)))
+se <- na.omit(sapply(simres, function(simres) simres$se))
+ci.hit <- na.omit(sapply(simres, function(simres) simres$ci.hit))
+tau.500 <- mean(tau)
+se.num.500 <- sd(tau)
+se.analyt.500 <- mean(se)
+ci.hit.500 <- mean(ci.hit)
+
+
+load('/Users/trifonovjuri/Desktop/pkg.sreg/sreg.git/data/unified/1000.RData')
+tau <- na.omit(as.numeric(sapply(simres, function(simres) simres$tau)))
+se <- na.omit(sapply(simres, function(simres) simres$se))
+ci.hit <- na.omit(sapply(simres, function(simres) simres$ci.hit))
+tau.1000 <- mean(tau)
+se.num.1000 <- sd(tau)
+se.analyt.1000 <- mean(se)
+ci.hit.1000 <- mean(ci.hit)
+
+load('/Users/trifonovjuri/Desktop/pkg.sreg/sreg.git/data/unified/2000.RData')
+tau <- na.omit(as.numeric(sapply(simres, function(simres) simres$tau)))
+se <- na.omit(sapply(simres, function(simres) simres$se))
+ci.hit <- na.omit(sapply(simres, function(simres) simres$ci.hit))
+tau.2000 <- mean(tau)
+se.num.2000 <- sd(tau)
+se.analyt.2000 <- mean(se)
+ci.hit.2000 <- mean(ci.hit)
+
+
+
+tau.vec <- c(tau.100, tau.300, tau.500, tau.1000,
+             tau.2000)
+se.num.vec <- c(se.num.100, se.num.300, se.num.500, se.num.1000,
+                se.num.2000)
+se.analyt.vec <- c(se.analyt.100, se.analyt.300, se.analyt.500, se.analyt.1000,
+                   se.analyt.2000)
+ci.hit.vec <- c(ci.hit.100, ci.hit.300, ci.hit.500, ci.hit.1000,
+                ci.hit.2000)
+sample.set <- c(100, 300, 500, 1000, 2000)
+
+# Uncomment if want to combine the results for large samples with the results for small samples
+#tau.vec <- c(tau.100, tau.200, tau.300, tau.400,
+#             tau.500, tau.750, tau.1000, tau.1500, 
+#             tau.2000, tau.3000, tau.4000, tau.5000)
+#se.num.vec <- c(se.num.100, se.num.200, se.num.300, se.num.400,
+#                se.num.500, se.num.750, se.num.1000, se.num.1500,
+#                se.num.2000, se.num.3000, se.num.4000, se.num.5000)
+#se.analyt.vec <- c(se.analyt.100, se.analyt.200, se.analyt.300, se.analyt.400,
+#                   se.analyt.500, se.analyt.750, se.analyt.1000, se.analyt.1500,
+#                   se.analyt.2000, se.analyt.3000, se.analyt.4000, se.analyt.5000)
+#ci.hit.vec <- c(ci.hit.100, ci.hit.200, ci.hit.300, ci.hit.400,
+#                ci.hit.500, ci.hit.750, ci.hit.1000, ci.hit.1500,
+#                ci.hit.2000, ci.hit.3000, ci.hit.4000, ci.hit.5000)
+#sample.set <- c(100, 200, 300, 400, 500, 750, 1000, 1500, 2000, 3000, 4000, 5000)
+
+graph.data <- data.frame('s.size' = sample.set, 'tau' = tau.vec, 'se.num' = se.num.vec, 'se.analyt' = se.analyt.vec, 'se.diff' = abs(se.num.vec - se.analyt.vec), 'ci.hit' = ci.hit.vec)
+
+ggplot(graph.data, aes(x = s.size, y = se.diff)) +
+  geom_line(size = 1, color = "red") + geom_point(size = 3, color = "red") +
+  labs(x = "Sample Size", y = "SE Difference") +
+  ggtitle("Graph of Sample Size vs. SE Difference")
+
+ggplot(graph.data) +
+  geom_line(aes(x = s.size, y = se.num, color = "SE Num"), size = 1) +
+  geom_point(aes(x = s.size, y = se.num, color = "SE Num"), size = 3) +
+  geom_line(aes(x = s.size, y = se.analyt, color = "SE Analyt"), size = 1) +
+  geom_point(aes(x = s.size, y = se.analyt, color = "SE Analyt"), size = 3) +
+  labs(x = "Sample Size", y = "SE") +
+  scale_color_manual(values = c("SE Num" = "blue", "SE Analyt" = "red")) +
+  theme_minimal()
+
+ggplot(graph.data, aes(x = s.size, y = ci.hit)) +
+  geom_line(color = 'red') + geom_point(color = 'red') +
+  labs(x = "Sample Size", y = "Coverage %") + scale_y_continuous(limits = c(0.7, 1))
+
